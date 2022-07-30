4b95d7479855503d4605e9552c3bd3359067a22e
iqsnider <iqsnider@gmail.com>
2022-07-29 22:09:14 -0400

    I learned how to render things in the terminal

--- render_py.py
+++ render_py.py
@@ -2,7 +2,16 @@
 
 import time
 
+x = 1
 for i in range(30):
-    print("---------\n---------\n---------\n")
+    ''' 
+    First part of print command sends cursor to start, middle prints images, 
+    end clears screen and repositions cursor.
+    '''
+    print("\x1b[H\n\n","\n----"+str(x)+"----\n----"+str(x)+"----\n----"+str(x)+"----\n","\x1b[H")
+    # print("---------\n----"+str(x)+"----\n---------\n")
+    
     time.sleep(0.25)
-    print("\x1b[6A")
+
+    x+=1
+    
--- render_t.c
+++ render_t.c
@@ -1,5 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+
+#include <unistd.h>
+
+#include <time.h>
 
 int main(){
-    printf("\x1b[23A");
+
+    putchar('\n');
+    for (int k = 0; k < 5; k++){
+        for (int j = 0; j < 10; j++){
+            for (int i = 0; i < 10; i++){
+                
+                putchar(j+'0');
+                usleep(15000);
+            }
+            putchar('\n');
+            
+        }
+        usleep(200000);
+        printf("\x1b[10A");
+    }
+    
 }
