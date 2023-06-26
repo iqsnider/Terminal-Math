50aa1dd454f9d9148528df11ae9acb442130bee5
iqsnider <iqsnider@gmail.com>
2023-06-26 12:08:55 -0400

    hyperbolic paraboloid

--- python/render3D.py
+++ python/render3D.py
@@ -5,8 +5,6 @@ import time
 '''
 TODO:
 1. algebraically define a range for the value, t.
-2. render 3D surfaces
-3. Add condition for t: if upos is outside of frame, don't add upos data to output
 4. luminance calculation
 5. runtime optimizations (it's real bad, gamers)
 6. Clean code
@@ -20,12 +18,17 @@ e_2 = np.array([0,0,1])
 nhat = np.array([1,0,0]) # vector normal to the screen
 
 
+# ray tracing?
+def ray_trace():
+    pass
+        
 # find luminance
-def luminance(r):
+def luminance(r, scale, zbuffer,rpos):
     # L = np.sqrt(r.dot(r))
+    
     L = np.dot(r, nhat)
 
-    luminance_index = int(L/1.7)
+    luminance_index = int(L*scale)
 
     return ".,-~:;=!*#$@"[luminance_index]
 
@@ -39,7 +42,7 @@ def decimal_range(start, stop, increment):
 # makes a rotation matrix from given Euler angles
 def make_rotation(phi, thta, psi):
 
-    # Euler rotation (convenient... sometimes.)
+    # #Euler rotation (convenient... sometimes.)
     # # rotation about z-axis
     # r_1 = np.array([[m.cos(phi), m.sin(phi), 0],
     #                [-m.sin(phi), m.cos(phi), 0],
@@ -55,22 +58,36 @@ def make_rotation(phi, thta, psi):
     #                [-m.sin(psi), m.cos(psi), 0],
     #                [0, 0, 1]])
 
-    # TAIT-BRYAN rotation (easier to visualize lol)
-    r_1 = np.array([[1,0,0],
-                    [0,m.cos(phi),-m.sin(phi)],
-                    [0,m.sin(phi),m.cos(phi)]])
+    # # TAIT-BRYAN rotation (easier to visualize lol)
+    # r_1 = np.array([[1,0,0],
+    #                 [0,m.cos(phi),-m.sin(phi)],
+    #                 [0,m.sin(phi),m.cos(phi)]])
     
-    r_2 = np.array([[m.cos(thta),0,m.sin(thta)],
-                    [0,1,0],
-                    [-m.sin(thta),0,m.cos(thta)]])
+    # r_2 = np.array([[m.cos(thta),0,m.sin(thta)],
+    #                 [0,1,0],
+    #                 [-m.sin(thta),0,m.cos(thta)]])
     
-    r_3 = np.array([[m.cos(psi),-m.sin(psi),0],
-                    [m.sin(psi),m.cos(psi),0],
-                    [0,0,1]])
+    # r_3 = np.array([[m.cos(psi),-m.sin(psi),0],
+    #                 [m.sin(psi),m.cos(psi),0],
+    #                 [0,0,1]])
+    
+    # consolidated rotation matrix
+    cosphi = m.cos(phi)
+    sinphi = m.sin(phi)
+
+    costhta = m.cos(thta)
+    sinthta = m.sin(thta)
+
+    cospsi = m.cos(psi)
+    sinpsi = m.sin(psi)
+
+    rotation_m = np.array([[costhta*cospsi, cospsi*sinthta*sinphi - sinpsi*cosphi, cospsi*sinthta*cosphi + sinpsi*sinphi],
+                          [sinpsi*costhta, sinpsi*sinthta*sinphi + cospsi*cosphi, sinpsi*sinthta*cosphi - cospsi*sinphi],
+                          [-sinthta, costhta*sinphi, costhta*cosphi]])
 
 
-    rotation_m = np.matmul(r_3, r_2)
-    rotation_m = np.matmul(rotation_m, r_1)
+    # rotation_m = np.matmul(r_3, r_2)
+    # rotation_m = np.matmul(rotation_m, r_1)
 
     return rotation_m
 
@@ -84,8 +101,8 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
     e_2_scale = frame_height
 
     # define graph tick mark scale (range of values shown on screen):
-    e_1tick_scale = frame_width
-    e_2tick_scale = frame_height
+    e_1tick_scale = 20
+    e_2tick_scale = 17
 
     # defines calcuation scales
     graph_height = frame_height/(e_2_scale/(e_2tick_scale))
@@ -96,6 +113,9 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
 
     # output matrix
     output = [ [' ']*frame_width for i in range(frame_height)]
+
+    # for luminance calculation (Thank you Andy Sloane!)
+    zbuffer = [ [0]*frame_width for i in range(frame_height)]
     
 
     # define elementary vectors without using Euler angles
@@ -109,84 +129,26 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
     y_axis = np.dot(rotation_m, e_1)
     z_axis = np.dot(rotation_m, e_2)
 
-    #################### Make the coordinate axes ####################
     if show_axes == True:
-
-        xlb, xub, xinc = -19, 19, 0.2
-        ylb, yub, yinc = -19, 19, 0.2
-        zlb, zub, zinc = -18, 18, 0.2
-
-        # x_axis
-        for t in decimal_range(xlb, xub, xinc):
-            r = x_axis*t
-
-            # needs work
-            rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
-                    int(frame_height/2 - r[2]/graph_height * frame_height)]
-
-            # print(upos, t)
-            output[rpos[1]][rpos[0]] = "x"
-
-        # y_axis
-        for t in decimal_range(ylb, yub, yinc):
-            r = y_axis*t
-
-            # needs work
-            rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
-                    int(frame_height/2 - r[2]/graph_height * frame_height)]
-
-            # print(upos, t)
-            output[rpos[1]][rpos[0]] = "y"
-
-        # z_axis
-        for t in decimal_range(zlb, zub, zinc):
-            r = z_axis*t
-
-            # needs work
-            rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
-                    int(frame_height/2 - r[2]/graph_height * frame_height)]
-
-            # print(upos, t)
-            output[rpos[1]][rpos[0]] = "z"
-        
         # define origin as center of the screen
         output[round(frame_height/2)][round(frame_width/2)] = "+"
 
-
-
-    #################### ######################## ####################
 
     ####################### A Function to Graph ######################
 
     '''The commented out stuff are functions that work, I just don't run them
     all at the same time for obvious reasons.'''
 
-    # --------> cone
-    # for theta in decimal_range(0, 2*np.pi, 0.02):
-    #     for r in decimal_range(0, m.sqrt(9), 0.02):
-            
-    #         x = r*m.cos(theta)
-    #         y = r*m.sin(theta)
-    #         z = m.sqrt(9 - r*r)
-
-    #         v = np.array([x,y,z])
-    #         r = np.dot(rotation_m, v)
-
-
-    #         rpos = [round(frame_width/2 + r[1]/graph_width * frame_width),
-    #             round(frame_height/2 - r[2]/graph_height * frame_height)]
-
-    #         output[rpos[1]][rpos[0]] = luminance(r)
-
     # -------> Torus
-    # R1 = 10
-    # R2 = 5
+    # R1 = 4
+    # R2 = 2
 
     # for theta in decimal_range(0, 2*np.pi, step):
+    #     costhta, sinthta = m.cos(theta), m.sin(theta)
     #     for phi in decimal_range(0, 2*np.pi, step):
             
-    #         x = (R1 + R2*m.cos(phi))*m.cos(theta)
-    #         y = (R1 + R2*m.cos(phi))*m.sin(theta)
+    #         x = (R1 + R2*m.cos(phi))*costhta
+    #         y = (R1 + R2*m.cos(phi))*sinthta
     #         z = R2*m.sin(phi)
 
     #         v = np.array([x,y,z])
@@ -195,15 +157,15 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
     #         rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
     #             int((frame_height/2 - r[2]/graph_height * frame_height))]
     
-    #         # output[rpos[1]][rpos[0]] = "#"
-    #         output[rpos[1]][rpos[0]] = luminance(r)
+    #         if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0: 
+    #             output[rpos[1]][rpos[0]] = "#" #luminance(r, 0.8, zbuffer, rpos)
 
 
 
     # --------> cube
-    # for t_x in decimal_range(-7, 7, step):
-    #     for t_y in decimal_range(-7,7, step):
-    #         for t_z in decimal_range(-7,7,step):
+    # for t_x in decimal_range(-4, 4, step):
+    #     for t_y in decimal_range(-4,4, step):
+    #         for t_z in decimal_range(-4,4,step):
             
     #             x = t_x
     #             y = t_y
@@ -216,7 +178,8 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
     #             rpos = [round(frame_width/2 + r[1]/graph_width * frame_width),
     #                 round(frame_height/2 - r[2]/graph_height * frame_height)]
 
-    #             output[rpos[1]][rpos[0]] = luminance(r)
+    #             if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+    #                 output[rpos[1]][rpos[0]] = "#" #luminance(r,0.8,zbuffer, rpos)
 
     # --------->  plane
     # for t_1 in decimal_range(-7, 7, 0.2):
@@ -236,8 +199,8 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
     #             output[rpos[1]][rpos[0]] = luminance(r)
 
     # ---------> a function
-    for t_1 in decimal_range(-12, 12, 0.2):
-        for t_2 in decimal_range(-12, 12, 0.2):
+    for t_1 in decimal_range(-7, 7, 0.2):
+        for t_2 in decimal_range(-7, 7, 0.2):
             x = t_1
             y = t_2
             z = t_1*t_2/10
@@ -249,33 +212,111 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
             rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
                 int(frame_height/2 - r[2]/graph_height * frame_height)]
             
-            output[rpos[1]][rpos[0]] = luminance(r)
+            if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+                output[rpos[1]][rpos[0]] = "#" #luminance(r,0.5, zbuffer, rpos)
+
+    # --------> trefoil knot
+    # R = 3.5
+    # for t in decimal_range(-4, 4, 0.01):
+    #     x = R*(m.sin(t) + 2*m.sin(2*t)) 
+    #     y = R*(m.cos(t) - 2*m.cos(2*t))
+    #     z =-R*m.sin(3*t)
+
+    #     v = np.array([x,y,z])
+    #     r = np.dot(rotation_m, v)
 
 
+    #     rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
+    #         int(frame_height/2 - r[2]/graph_height * frame_height)]
+        
+    #     if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+    #         output[rpos[1]][rpos[0]] = luminance(r,0.5, zbuffer, rpos)
 
+    # R = 3.5
+    # for t in decimal_range(-4, 4, 0.01):
+    #     x = R*(m.sin(t) + 2*m.sin(2*t)) +1
+    #     y = R*(m.cos(t) - 2*m.cos(2*t)) +1
+    #     z =-R*m.sin(3*t) +1
+
+    #     v = np.array([x,y,z])
+    #     r = np.dot(rotation_m, v)
+
+
+    #     rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
+    #         int(frame_height/2 - r[2]/graph_height * frame_height)]
+        
+    #     if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+    #         output[rpos[1]][rpos[0]] = luminance(r,0.5, zbuffer, rpos)
 
 
 
     #################### TEST ####################
 
-    # test line:
-    # lb = -15
-    # ub = 15
+    # # test line:
+    # lb = -10
+    # ub = 10
     # increment = 0.02
 
     # # draw line (add line position data to output matrix)
     # for t in decimal_range(lb, ub, increment):
 
-    #     v = np.array([t,6*m.cos(t),6*m.sin(t)])
+    #     v = np.array([t,2*m.cos(t),2*m.sin(t)])
     #     r = np.dot(rotation_m, v)
 
     #     # needs work
     #     rpos = [round(frame_width/2 + r[1]/graph_width * frame_width),
     #             round(frame_height/2 - r[2]/graph_height * frame_height)]
-
-    #     output[rpos[1]][rpos[0]] = '#'
+    #     if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+    #         output[rpos[1]][rpos[0]] = '#'
 
     ################### END TEST ###################
+
+    #################### Make the coordinate axes ####################
+    if show_axes == True:
+
+        # xlb, xub, xinc = -20, 20, 0.2
+        # ylb, yub, yinc = -20, 20, 0.2
+        # zlb, zub, zinc = -20, 20, 0.2
+        alb, aub, ainc = -graph_height/2,graph_height/2, 0.2
+
+        # x_axis
+        for t in decimal_range(alb, aub, ainc):
+            r = x_axis*t
+
+            # needs work
+            rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
+                    int(frame_height/2 - r[2]/graph_height * frame_height)]
+
+            if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+                output[rpos[1]][rpos[0]] = "x"
+
+        # y_axis
+        for t in decimal_range(alb, aub, ainc):
+            r = y_axis*t
+
+            # needs work
+            rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
+                    int(frame_height/2 - r[2]/graph_height * frame_height)]
+
+            if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+                output[rpos[1]][rpos[0]] = "y"
+
+        # z_axis
+        for t in decimal_range(alb, aub, ainc):
+            r = z_axis*t
+
+            # needs work
+            rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
+                    int(frame_height/2 - r[2]/graph_height * frame_height)]
+
+            if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+                output[rpos[1]][rpos[0]] = "z"
+        
+        
+
+
+
+    #################### ######################## ####################
     
     # make frame
     for k in range(0, frame_height):
@@ -302,7 +343,7 @@ if __name__ == '__main__':
     # psi = 0
 
     propogate = True
-    show_axes = True
+    show_axes = True 
     step = 0.5
 
     if propogate == True:
@@ -315,7 +356,7 @@ if __name__ == '__main__':
             rotation_m = make_rotation(phi, thta, psi)
 
             render_frame(frame_height, frame_width, rotation_m, step, show_axes)
-            print("\x1b[41A")
+            print("\x1b["+ str(frame_height+1) +"A")
             usleep(50000)
             
 
