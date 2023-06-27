e59ebaa1330c23f8d6cda1a9658c5912c2135691
iqsnider <iqsnider@gmail.com>
2023-06-26 21:10:33 -0400

    cleaning. Graphing stuff has moved to a new repo.

--- python/render3D.py
+++ python/render3D.py
@@ -124,152 +124,34 @@ def render_frame(frame_height, frame_width, rotation_m, step, show_axes):
     nhat = np.array([1,0,0]) # vector normal to the screen
 
 
-    # show coordinate axis
-    x_axis = np.dot(rotation_m, nhat)
-    y_axis = np.dot(rotation_m, e_1)
-    z_axis = np.dot(rotation_m, e_2)
 
     if show_axes == True:
+        # show coordinate axis
+        x_axis = np.dot(rotation_m, nhat)
+        y_axis = np.dot(rotation_m, e_1)
+        z_axis = np.dot(rotation_m, e_2)
         # define origin as center of the screen
         output[round(frame_height/2)][round(frame_width/2)] = "+"
 
 
     ####################### A Function to Graph ######################
 
-    '''The commented out stuff are functions that work, I just don't run them
-    all at the same time for obvious reasons.'''
-
-    # -------> Torus
-    # R1 = 4
-    # R2 = 2
-
-    # for theta in decimal_range(0, 2*np.pi, step):
-    #     costhta, sinthta = m.cos(theta), m.sin(theta)
-    #     for phi in decimal_range(0, 2*np.pi, step):
-            
-    #         x = (R1 + R2*m.cos(phi))*costhta
-    #         y = (R1 + R2*m.cos(phi))*sinthta
-    #         z = R2*m.sin(phi)
-
-    #         v = np.array([x,y,z])
-    #         r = np.dot(rotation_m, v)
-
-    #         rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
-    #             int((frame_height/2 - r[2]/graph_height * frame_height))]
-    
-    #         if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0: 
-    #             output[rpos[1]][rpos[0]] = "#" #luminance(r, 0.8, zbuffer, rpos)
-
-
-
-    # --------> cube
-    # for t_x in decimal_range(-4, 4, step):
-    #     for t_y in decimal_range(-4,4, step):
-    #         for t_z in decimal_range(-4,4,step):
-            
-    #             x = t_x
-    #             y = t_y
-    #             z = t_z
-
-    #             v = np.array([x,y,z])
-    #             r = np.dot(rotation_m, v)
-
-
-    #             rpos = [round(frame_width/2 + r[1]/graph_width * frame_width),
-    #                 round(frame_height/2 - r[2]/graph_height * frame_height)]
-
-    #             if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
-    #                 output[rpos[1]][rpos[0]] = "#" #luminance(r,0.8,zbuffer, rpos)
-
-    # --------->  plane
-    # for t_1 in decimal_range(-7, 7, 0.2):
-    #     for t_2 in decimal_range(-7,7, 0.2):
-            
-    #             x = 0
-    #             y = t_1
-    #             z = t_2
-
-    #             v = np.array([x,y,z])
-    #             r = np.dot(rotation_m, v)
-
-
-    #             rpos = [round(frame_width/2 + r[1]/graph_width * frame_width),
-    #                 round(frame_height/2 - r[2]/graph_height * frame_height)]
-
-    #             output[rpos[1]][rpos[0]] = luminance(r)
-
-    # ---------> a function
-    for t_1 in decimal_range(-7, 7, 0.2):
-        for t_2 in decimal_range(-7, 7, 0.2):
-            x = t_1
-            y = t_2
-            z = t_1*t_2/10
-
-            v = np.array([x,y,z])
-            r = np.dot(rotation_m, v)
-
-
-            rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
-                int(frame_height/2 - r[2]/graph_height * frame_height)]
-            
-            if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
-                output[rpos[1]][rpos[0]] = "#" #luminance(r,0.5, zbuffer, rpos)
-
     # --------> trefoil knot
-    # R = 3.5
-    # for t in decimal_range(-4, 4, 0.01):
-    #     x = R*(m.sin(t) + 2*m.sin(2*t)) 
-    #     y = R*(m.cos(t) - 2*m.cos(2*t))
-    #     z =-R*m.sin(3*t)
+    R = 3.5
+    for t in decimal_range(-4, 4, 0.01):
+        x = R*(m.sin(t) + 2*m.sin(2*t)) 
+        y = R*(m.cos(t) - 2*m.cos(2*t))
+        z =-R*m.sin(3*t)
 
-    #     v = np.array([x,y,z])
-    #     r = np.dot(rotation_m, v)
+        v = np.array([x,y,z])
+        r = np.dot(rotation_m, v)
 
 
-    #     rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
-    #         int(frame_height/2 - r[2]/graph_height * frame_height)]
+        rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
+            int(frame_height/2 - r[2]/graph_height * frame_height)]
         
-    #     if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
-    #         output[rpos[1]][rpos[0]] = luminance(r,0.5, zbuffer, rpos)
-
-    # R = 3.5
-    # for t in decimal_range(-4, 4, 0.01):
-    #     x = R*(m.sin(t) + 2*m.sin(2*t)) +1
-    #     y = R*(m.cos(t) - 2*m.cos(2*t)) +1
-    #     z =-R*m.sin(3*t) +1
-
-    #     v = np.array([x,y,z])
-    #     r = np.dot(rotation_m, v)
-
-
-    #     rpos = [int(frame_width/2 + r[1]/graph_width * frame_width),
-    #         int(frame_height/2 - r[2]/graph_height * frame_height)]
-        
-    #     if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
-    #         output[rpos[1]][rpos[0]] = luminance(r,0.5, zbuffer, rpos)
-
-
-
-    #################### TEST ####################
-
-    # # test line:
-    # lb = -10
-    # ub = 10
-    # increment = 0.02
-
-    # # draw line (add line position data to output matrix)
-    # for t in decimal_range(lb, ub, increment):
-
-    #     v = np.array([t,2*m.cos(t),2*m.sin(t)])
-    #     r = np.dot(rotation_m, v)
-
-    #     # needs work
-    #     rpos = [round(frame_width/2 + r[1]/graph_width * frame_width),
-    #             round(frame_height/2 - r[2]/graph_height * frame_height)]
-    #     if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
-    #         output[rpos[1]][rpos[0]] = '#'
-
-    ################### END TEST ###################
+        if rpos[0] < frame_width and rpos[1] < frame_height and rpos[0] >=0 and rpos[1] >= 0:
+            output[rpos[1]][rpos[0]] = "#" #luminance(r,0.5, zbuffer, rpos)
 
     #################### Make the coordinate axes ####################
     if show_axes == True:
@@ -344,7 +226,7 @@ if __name__ == '__main__':
 
     propogate = True
     show_axes = True 
-    step = 0.5
+    step = 0.2
 
     if propogate == True:
         while True:
@@ -365,10 +247,3 @@ if __name__ == '__main__':
         rotation_m = make_rotation(phi, thta, psi)
 
         render_frame(frame_height, frame_width, rotation_m, step, show_axes)
-
-
-
-    # speed check
-    # end_time = time.time()
-    # elapsed_time = end_time - start_time
-    # print(elapsed_time)
